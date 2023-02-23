37d1275b31ef8dc9b6cc1f3b2ed5b85419f0366b
Yesim Dundar <kuranyesim@gmail.com>
2023-02-23 11:24:05 +0100

    finnished

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,36 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    # the ugliest way
+    # @review = Review.new(review_params)
+    # @review.restaurant_id = @restaurant.id
+
+    # # less ugly way
+    # @review = Review.new(review_params)
+    # @review.restaurant = @restaurant
+
+    # the best practice
+    @review = @restaurant.reviews.new(review_params)
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_155245) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
